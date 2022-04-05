64fa28674f1e295b5da0d282d709d13e0f6daf92
Navid <Navid_mon@yahoo.com>
2022-04-05 06:57:32 +0430

    add card

--- Walletino.Web/clientapp/src/components/app/account/comps/viewAccount.tsx
+++ Walletino.Web/clientapp/src/components/app/account/comps/viewAccount.tsx
@@ -14,15 +14,15 @@ const ViewAccount = () => {
             <div className="accounts">
                 <div className="account-card">
                     <div className="row-item">
-
+                        <span>Title</span> : <span>Credit Bank</span>
                     </div>
-                </div>
-                <div className="account-card">
-
-                </div>
-                <div className="account-card">
-
-                </div>
+                    <div className="row-item">
+                        <span>Number</span> : <span>235-33693-3655</span>
+                    </div>
+                    <div className="row-item">
+                        <span>Amount</span> : <span>15000$</span>
+                    </div>
+                </div> 
             </div>
         </>
     )
